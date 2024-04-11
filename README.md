ddb8764ddfa14671e35eb119ee76f21365fdad0b
yleniapregnolato <yleniapregnolato@gmail.com>
2024-04-11 16:55:46 +0200

    feat: print informations in console

--- README.md
+++ README.md
@@ -3,10 +3,10 @@
 ## Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
 
 ### MILESTON 0:
-- [ ] creare l'array di oggetti con le informazioni fornite
+- [x] creare l'array di oggetti con le informazioni fornite
 
 ### MILESTONE 1:
-- [ ] Stampare su console le informazioni di nome, ruolo e la stringa della foto per ogni membro del team
+- [x] Stampare su console le informazioni di nome, ruolo e la stringa della foto per ogni membro del team
 
 ### MILESTONE 2:
 - [ ] Stampare le stesse informazioni su DOM sottoforma di stringhe
--- js/script.js
+++ js/script.js
@@ -6,41 +6,55 @@ const team = [
     {
         name: "Wayne Barnett",
         profession: "Founder & CEO",
-        pic: "wayne-barnett-founder-ceo.jpg";
-    }
+        pic: "wayne-barnett-founder-ceo.jpg",
+    },
 
     {
         name: "Angela Caroll",
         profession: "Chief Editor",
-        pic: "angela-caroll-chief-editor.jpg";
-    }
+        pic: "angela-caroll-chief-editor.jpg",
+    },
 
     {
         name: "Walter Gordon",
         profession: "Office Manager",
-        pic: "walter-gordon-office-manager.jpg";
-    }
+        pic: "walter-gordon-office-manager.jpg",
+    },
 
     {
         name: "Angela Lopez",
         profession: "Social Media Manager",
-        pic: "angela-lopez-social-media-manager.jpg";
-    }
+        pic: "angela-lopez-social-media-manager.jpg",
+    },
 
     {
         name: "Scott Estrada",
         profession: "Developer",
-        pic: "scott-estrada-developer.jpg";
-    }
+        pic: "scott-estrada-developer.jpg",
+    },
 
 
     {
         name: "Barbara Ramos",
         profession: "Graphic Designer",
-        pic: "barbara-ramos-graphic-designer.jpg";
+        pic: "barbara-ramos-graphic-designer.jpg",
     }
 
 
-]
+];
 
+console.log(team);
 
+// MILESTONE 1
+
+// stampo su console le informazioni dei mebri del team
+
+for (let i = 0; i < team.length; i++) {
+    const curTeam = team[i];
+    console.log(curTeam);
+    // stampo utilizzando dot.notation
+    
+    console.log(curTeam.name);
+    console.log(curTeam.profession);
+    console.log(curTeam.pic);
+}
