34658ce9518a2d58ed4f7d90dcd1bf0527edb531
yleniapregnolato <yleniapregnolato@gmail.com>
2024-04-11 16:48:46 +0200

    feat: define team's array

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// MILESTONE 0
+
+// definisco un array di oggetti, ogni oggetto rappresenta un membro del team con tutti i suoi dati
+
+const team = [
+    {
+        name: "Wayne Barnett",
+        profession: "Founder & CEO",
+        pic: "wayne-barnett-founder-ceo.jpg";
+    }
+
+    {
+        name: "Angela Caroll",
+        profession: "Chief Editor",
+        pic: "angela-caroll-chief-editor.jpg";
+    }
+
+    {
+        name: "Walter Gordon",
+        profession: "Office Manager",
+        pic: "walter-gordon-office-manager.jpg";
+    }
+
+    {
+        name: "Angela Lopez",
+        profession: "Social Media Manager",
+        pic: "angela-lopez-social-media-manager.jpg";
+    }
+
+    {
+        name: "Scott Estrada",
+        profession: "Developer",
+        pic: "scott-estrada-developer.jpg";
+    }
+
+
+    {
+        name: "Barbara Ramos",
+        profession: "Graphic Designer",
+        pic: "barbara-ramos-graphic-designer.jpg";
+    }
+
+
+]
+
+
